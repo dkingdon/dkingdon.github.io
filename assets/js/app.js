578763d8000fc3edcce46cc2f9e3535148fa619a
dkingdon <daniel.kingdon@gmail.com>
2016-10-25 23:15:49 -0700

    Header text animation js and adjusted h1 and p font size and placement

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,21 @@
 // sanity check
 console.log("app.js linked.");
+
+
+// window.onload = function(){
+//   // Change this value to however many seconds you want to delay the text by.
+//   var theDelay = 13;
+//   var timer = setTimeout("showText()",theDelay*1000)
+// }
+// function showText(){
+//   document.getElementById("delayedTextH1").style.visibility = "visible";
+// }
+
+function showText(id,delay){
+var elem=document.getElementById(id);
+setTimeout(function(){elem.style.visibility='visible';},delay*1000)
+}
+window.onload=function(){
+showText('delayedTextH1',8);
+showText('delayedTextP',10);
+}
