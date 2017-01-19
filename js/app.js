d4c5337a7316e9de698f801a01f348cd89bd9368
dkingdon <daniel.kingdon@gmail.com>
2017-01-19 10:24:58 -0800

    added Rails project (commented out) and fixed the github link to the Game Break app

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
 
     {name:'Game Break',
     url: $sce.trustAsResourceUrl('https://peaceful-retreat-83581.herokuapp.com/'),
-    github: '',
+    github: 'https://github.com/dkingdon/game_break',
     image: '../images/game-break.png',
     desc: 'Game Break is a game hub consisting of three mini games. Single page Angular app with a bootstrap style responsive design meant to be played on both web or mobile platforms.  Technologies: AJAX, jQuery, Angular, Bootstrap, Express, MongoDB, Node '},
 
@@ -25,17 +25,17 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
     image: '../images/trail-finder.png',
     desc: 'Application designed to help mountain bike riders new to the SF Bay area find trails to ride based on their skill level. Utilizing Google Maps API. Fully CRUD-able Express API adhering to RESTful Routes standards, using AJAX, jQuery, and  Handlebars for templating. All supported with a MongoDB database using Mongoose. Go to: /trails/admin to create and delete'},
 
-
-    // {name:'TBD',
-    // url: $sce.trustAsResourceUrl('#'),
-    // github: '#',
-    // image: '#',
-    // desc: '#'}
+    /* Need to figure out how to get rails app working */
+    {name:'Paregrination',
+    url: $sce.trustAsResourceUrl('https://peaceful-sierra-36805.herokuapp.com/'),
+    github: 'https://github.com/yasuper8/peregrination',
+    image: '../images/trail-finder.png',
+    desc: 'A Ruby on Rails app designed to empower people from all over the world to leave comments and suggestions for travelers on a city by city basis. Travelers can then see where they could visit and where they should avoid. Ruby on rails, postgresql, crypt, using bootstrap, and hosted on heroku.'}
 
   ]
 
   $scope.getSite = function(i) {
-    console.log($scope.sites[i].url);
+    // console.log($scope.sites[i].url);
     $scope.url = $scope.sites[i].url;
     $scope.github = $scope.sites[i].github;
     $scope.name = $scope.sites[i].name;
